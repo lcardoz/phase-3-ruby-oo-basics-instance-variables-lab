17b4fad4273f2fc93366770b633f8d6ba5bed282
Leah Cardoz <leahcardoz@gmail.com>
2022-11-21 21:01:12 -0500

    commit tree 2247b5530cc070069ae6c82faec3ea0752860cfd

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
 
+    # setter method using instance variable
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+
+    # getter method using instance variable
+    def name
+        @this_dogs_name
+    end
+
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+lassie.name
